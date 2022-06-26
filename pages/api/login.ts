97cc3145fddd6f933208889b7765f19d2175ee82
guaxelo <silvaugusto222@gmail.com>
2022-06-26 16:07:23 -0300

    Update login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -11,7 +11,7 @@ export default (
             senha === 'Admin@123'){
                 res.status(200).json({msg: 'Usuario autenticado com sucesso'})  
         }
-        return res.status(405).json({erro: 'Usuario ou senha não encontrado'})
+        return res.status(400).json({erro: 'Usuario ou senha não encontrado'})
     }
     return res.status(405).json({erro: 'metodo informado não é valido'})
 }
