61ff58ad96d0a9dfa85bca83a01a1b4f4fb1fe89
guaxelo <silvaugusto222@gmail.com>
2022-06-29 06:53:48 -0300

    intergração cosmic e multer

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -4,43 +4,60 @@ import type {CadastroRequisicao} from '../../types/CadastroRequisicao'
 import {UsuarioModel} from '../../models/UsuarioModel'
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
 import md5 from 'md5'
+import {upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic'
+import nc from 'next-connect'
+import multer from 'multer'
+import { isModuleNamespaceObject } from 'util/types'
 
-const endpointCadastro =
-    async (req: NextApiRequest, res: NextApiResponse<ResportaPadraoMsg>) =>{
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req: NextApiRequest, res: NextApiResponse<ResportaPadraoMsg>) =>{
+        try{
+            const usuario = req.body as CadastroRequisicao
+        
+            if(!usuario.nome || usuario.nome.length < 2){
+                return res.status(400).json({erro: 'Nome invalido'})
+            }
+            if(!usuario.email || usuario.email.length < 5 
+                || !usuario.email.includes('@')
+                || !usuario.email.includes('.')){
+                return res.status(400).json({erro: 'Email invalido'})
+            }
+            if(!usuario.senha || usuario.senha.length < 4){
+                return res.status(400).json({erro: 'Senha invalida'})
+            }
     
-    if(req.method === 'POST'){
-       const usuario = req.body as CadastroRequisicao
+            //validação de duplicidade de usuario
+            const usuarioComMesmoEmail = await UsuarioModel.find({email : usuario.email})
+            if(usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0){
+                return res.status(400).json({erro: 'Essa conta ja existe'})
+    
+            }
+            // enviar a imagem do multer para o cosmic
+            const image = await uploadImagemCosmic(req)
+    
+            // salvar no banco de dados
+            const usuarioASerSalvo = {
+                nome : usuario.nome,
+                email : usuario.email,
+                senha : usuario.senha,
+                avatar : image?.media?.url
+            }
+            await UsuarioModel.create(usuarioASerSalvo)
+            return res.status(200).json({msg: 'Usuario criado com sucesso!'})
+
+            }catch(e : any){
+                console.log(e);
+                return res.status(400).json({erro : e.toString()})
+            }    
+})
+    
        
-       if(!usuario.nome || usuario.nome.length < 2){
-            return res.status(400).json({erro: 'Nome invalido'})
-       }
-       if(!usuario.email || usuario.email.length < 5 
-            || !usuario.email.includes('@')
-            || !usuario.email.includes('.')){
-            return res.status(400).json({erro: 'Email invalido'})
-        }
-        if(!usuario.senha || usuario.senha.length < 4){
-            return res.status(400).json({erro: 'Senha invalida'})
-        }
 
-        //validação de duplicidade de usuario
-        const usuarioComMesmoEmail = await UsuarioModel.find({email : usuario.email})
-        if(usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0){
-            return res.status(400).json({erro: 'Essa conta ja existe'})
-
-        }
-
-        // salvar no banco de dados
-        const usuarioASerSalvo = {
-            nome : usuario.nome,
-            email : usuario.email,
-            senha : usuario.senha,
-        }
-
-        await UsuarioModel.create(usuario)
-        return res.status(200).json({msg: 'Usuario criado com sucesso!'})
+export const config = {
+    api: {
+        bodyParser : false
     }
-    return res.status(405).json({erro: 'metodo informado não é valido'})
-    }
+}
 
-export default conectarMongoDB(endpointCadastro)
+export default conectarMongoDB(handler)
